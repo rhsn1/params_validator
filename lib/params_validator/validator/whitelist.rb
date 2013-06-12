cced6b14bcf8e365e7c1e67327abbd422f9f8a40
Anton Lindqvist <anton@qvister.se>
2013-06-12 16:56:48 +0200

    Implemented whitelist logic

--- lib/params_validator/validator/whitelist.rb
+++ lib/params_validator/validator/whitelist.rb
@@ -2,14 +2,19 @@ module ParamsValidator
   module Validator
     class Whitelist
       def initialize(definition)
+        @whitelist = definition.delete(:_whitelist).map(&:to_s).to_set
       end
 
       def error_message
       end
 
       def valid?(value)
-        true
+        whitelist.include?(value)
       end
+
+      private
+
+      attr_reader :whitelist
     end
   end
 end
